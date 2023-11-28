e2a5b5e704bb3d878611eaaec98aed41802bcf80
abaiao-r <andrefranciscorolao@gmail.com>
2023-11-28 13:34:22 +0000

    Update Server.hpp, ServerCommands.cpp, list.sh, and add who.sh and Joao.sh scripts
    1. Updated Server.hpp and ServerCommands.cpp:
       - Updated the Updated timestamps in the comments.
       - Refactored the cmd_list and cmd_who functions, added comments for clarity.
       - Modified cmd_mode to improve readability and fix minor issues.
    
    2. Modified list.sh:
       - Added comments explaining the use of the -e option for terminator.
    
    3. Added who.sh and Joao.sh scripts:
       - who.sh is a script to run Joao.sh in a new xterm window.
       - Joao.sh simulates a client connecting to the server, joining a channel, and executing WHO commands.
    
    4. Minor adjustments and comments for better code readability.
    
    Changes aim to enhance code clarity, improve script comments, and address minor issues.

--- Irc2BRANCH/scripts/list.sh
+++ Irc2BRANCH/scripts/list.sh
@@ -5,6 +5,9 @@
 # Run Andre.sh in a new xterm window
 terminator -e "./list/Andre.sh" &
 
+#-e is to execute the command in the new terminal
+#to print the comand on the terminal use -x example: terminator -x "./list/Andre.sh" &
+
 
 # Run Gabriel.sh in a new xterm window
 terminator --new-tab -e "./list/Gabriel.sh" &
---
+++ Irc2BRANCH/scripts/who.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script to run Joao.sh in a new xterm window
+
+# Run Andre.sh in a new xterm window
+terminator -e "./who/Joao.sh" &
